b25be0c54481b53a65b5eb24297a9540cc662f7d
teters <mpatrone@correo.um.edu.uy>
2025-06-10 09:22:48 -0300

    packaging done

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
+ 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1000,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'), // Si usas un preload
+            //preload: path.join(__dirname, 'preload.js'), // Si usas un preload
             contextIsolation: true,
         },
     });
-
+ 
     win.loadURL('http://localhost:3000'); // Cargar la app de React
+ 
+    win.webContents.openDevTools();
 }
-
+ 
 app.whenReady().then(createWindow);
-
+ 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-
+ 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
---
+++ preload.js
@@ -0,0 +1,4 @@
+// preload.js
+window.addEventListener('DOMContentLoaded', () => {
+    // Aquí puedes agregar código para el contexto de la ventana
+});
